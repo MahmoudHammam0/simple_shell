757be38a953dbe14218e86c649530a2b146983b5
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-08 04:44:45 -0700

    task 1 & 2

--- main.h
+++ main.h
@@ -9,10 +9,8 @@
 #include <sys/wait.h>
 extern char **environ;
 char *get_input(void);
-int num_tok(char *s);
 char **tokenizer(char *input);
 int _strlen(char *str);
 char *_strcpy(char *str);
-void free_func(char **cmd);
 int executioner(char **cmd, char **argv, char *input);
 #endif
